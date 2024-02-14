4189d89ac51186ba4dfa655ce6969189270eaf3e
Simona Ragauskaitė <simonaaragauskaite@gmail.com>
2024-02-14 23:30:26 +0200

    fixed navigation

--- src/HomeChoreTracker.Portal/HomeChoreTracker.Portal/Pages/Home/HomeMembers.cshtml.cs
+++ src/HomeChoreTracker.Portal/HomeChoreTracker.Portal/Pages/Home/HomeMembers.cshtml.cs
@@ -1,16 +1,16 @@
-using HomeChoreTracker.Portal.Models.Home;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HomeChoreTracker.Portal.Pages.Home
 {
-    public class IndexModel : PageModel
+    public class HomeMembersModel : PageModel
     {
         [BindProperty]
         public int Id { get; set; }
         public void OnGet(int id)
         {
             Id = id;
+            // You can use the id parameter as needed
         }
     }
 }
--- src/HomeChoreTracker.Portal/HomeChoreTracker.Portal/Pages/Homes/Invite.cshtml.cs
+++ src/HomeChoreTracker.Portal/HomeChoreTracker.Portal/Pages/Homes/Invite.cshtml.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
 using System.Net;
 
-namespace HomeChoreTracker.Portal.Pages.Home
+namespace HomeChoreTracker.Portal.Pages.Homes
 {
     public class InviteModel : PageModel
     {
