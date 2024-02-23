1037b2b7d0b633693a25f8fef2bf651515ea882a
Simona Ragauskaitė <simonaaragauskaite@gmail.com>
2024-02-24 00:56:44 +0200

    display advices

--- src/HomeChoreTracker.Portal/HomeChoreTracker.Portal/Pages/Forum/Index.cshtml.cs
+++ src/HomeChoreTracker.Portal/HomeChoreTracker.Portal/Pages/Forum/Index.cshtml.cs
@@ -1,12 +1,45 @@
+using HomeChoreTracker.Portal.Models.Forum;
+using HomeChoreTracker.Portal.Models.HomeChoreBase;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
 
 namespace HomeChoreTracker.Portal.Pages.Forum
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
-        }
-    }
+		private readonly IHttpClientFactory _httpClientFactory;
+		private readonly IConfiguration _config;
+
+		public List<AdviceResponse> Advices { get; set; }
+
+		public IndexModel(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+		{
+			_httpClientFactory = httpClientFactory;
+			_config = configuration;
+		}
+
+		public async Task<IActionResult> OnGetAsync()
+		{
+			var token = User.FindFirstValue("Token");
+			using (var httpClient = _httpClientFactory.CreateClient())
+			{
+				httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+				var apiUrl = $"{_config["ApiUrl"]}/Forum";
+
+				var response = await httpClient.GetAsync(apiUrl);
+
+				if (response.IsSuccessStatusCode)
+				{
+					Advices = await response.Content.ReadFromJsonAsync<List<AdviceResponse>>();
+					return Page();
+				}
+				else
+				{
+					return BadRequest($"Failed to retrieve data: {response.ReasonPhrase}");
+				}
+			}
+		}
+	}
 }
