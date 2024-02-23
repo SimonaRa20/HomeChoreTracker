a93720f0e98bffda6d211f27f4c0b2e180c85e32
Simona Ragauskaitė <simonaaragauskaite@gmail.com>
2024-02-24 01:06:19 +0200

    added search by text

--- src/HomeChoreTracker.Portal/HomeChoreTracker.Portal/Pages/Forum/Index.cshtml.cs
+++ src/HomeChoreTracker.Portal/HomeChoreTracker.Portal/Pages/Forum/Index.cshtml.cs
@@ -2,44 +2,57 @@ using HomeChoreTracker.Portal.Models.Forum;
 using HomeChoreTracker.Portal.Models.HomeChoreBase;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using System.Security.Claims;
 
 namespace HomeChoreTracker.Portal.Pages.Forum
 {
     public class IndexModel : PageModel
     {
-		private readonly IHttpClientFactory _httpClientFactory;
-		private readonly IConfiguration _config;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _config;
 
-		public List<AdviceResponse> Advices { get; set; }
+        public List<AdviceResponse> Advices { get; set; }
 
-		public IndexModel(IHttpClientFactory httpClientFactory, IConfiguration configuration)
-		{
-			_httpClientFactory = httpClientFactory;
-			_config = configuration;
-		}
+        public IndexModel(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _httpClientFactory = httpClientFactory;
+            _config = configuration;
+        }
 
-		public async Task<IActionResult> OnGetAsync()
-		{
-			var token = User.FindFirstValue("Token");
-			using (var httpClient = _httpClientFactory.CreateClient())
-			{
-				httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+        public async Task<IActionResult> OnGetAsync(string search)
+        {
+            var token = User.FindFirstValue("Token");
+            using (var httpClient = _httpClientFactory.CreateClient())
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
 
-				var apiUrl = $"{_config["ApiUrl"]}/Forum";
+                var apiUrl = $"{_config["ApiUrl"]}/Forum";
 
-				var response = await httpClient.GetAsync(apiUrl);
+                var response = await httpClient.GetAsync(apiUrl);
 
-				if (response.IsSuccessStatusCode)
-				{
-					Advices = await response.Content.ReadFromJsonAsync<List<AdviceResponse>>();
-					return Page();
-				}
-				else
-				{
-					return BadRequest($"Failed to retrieve data: {response.ReasonPhrase}");
-				}
-			}
-		}
-	}
+                if (response.IsSuccessStatusCode)
+                {
+                    Advices = await response.Content.ReadFromJsonAsync<List<AdviceResponse>>();
+
+                    // Filter advices based on the search query
+                    if (!string.IsNullOrEmpty(search))
+                    {
+                        Advices = Advices.Where(a => a.Title.Contains(search) || a.Description.Contains(search)).ToList();
+                    }
+
+                    return Page();
+                }
+                else
+                {
+                    return BadRequest($"Failed to retrieve data: {response.ReasonPhrase}");
+                }
+            }
+        }
+    }
 }
